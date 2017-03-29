5b17f5149d576f39b88792242bb89bf979423d56
devalexer <alliemckay.dev@gmail.com>
2017-03-29 16:29:57 -0400

    Translates and returns in Morse Code

--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace MorseCode
             var morseCodeDictionary = new Dictionary<char, string>();
 
             //save morse.csv file path
-            var morseCodeCSV = "morse.cvs";
+            var morseCodeCSV = "morse.csv";
 
             // access .csv file and read into program with streamreader
             using (var reader = new StreamReader(morseCodeCSV))
@@ -29,30 +30,32 @@ namespace MorseCode
                     var line = reader.ReadLine().Split(','); // ["S","..."]
 
                     //add the split string into the dictionary
-                    morseCodeDictionary.Add(line[0].ToString(), line[1]);
+                    morseCodeDictionary.Add(char.Parse(line[0]), line[1]);
                 }
             }
 
-            //split strings in dictionary to char and strings??
+            //split strings in dictionary to char and strings???
 
             //request user input
             Console.WriteLine("What would you like translated to morse code?");
-            string toBeTranslated = Console.ReadLine();
+            string toBeTranslated = Console.ReadLine().ToUpper();
 
-            //  *  Using the dictionary created, translate the inputed string into morse code
+            //  Directions: Using the dictionary created, translate the inputed string into morse code
             //break apart user input to chars
             
             //for each letter in user input, return equivalent character from dictionary
-            foreach (var line in morseCodeDictionary)
+            foreach (var indivLetter in toBeTranslated)
             {
-
+                Console.WriteLine(morseCodeDictionary[indivLetter]);
             }
 
 
 
 
             //You need to display that string back to the user in Morse Code
-
+            //Save the users input to a CSV as both the original string and encoded.
+            //Ask the user if they have any more messages that they want to encode
+            //repeat steps 2 - 6 until the user elects to step
 
 
         }
